f0263f89f492066df4f1ab2b79ec2e47c0c396e7
Stamatis Karras <skarras@c1r3p3.hive.fi>
2024-11-12 09:18:57 +0200

    Added 2 more bonus functions

---
+++ Unfinished_Functions/ft_lstclear_bonus.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		del((*lst)->content);
+		del((*lst)->next);
+		*lst = tmp;
+	}
+	free
+	*lst = NULL;
+}
