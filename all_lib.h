8272f62e09d287edcebd8a7cf816abcec7209ba3
Stamatis Karras <skarras@c1r3p1.hive.fi>
2025-01-20 11:35:31 +0200

    Added printf and get_next_line

---
+++ all_lib.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   all_lib.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarras <skarras@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 11:31:18 by skarras           #+#    #+#             */
+/*   Updated: 2025/01/20 11:31:27 by skarras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALL_LIB_H
+# define ALL_LIB_H
+
+# include "libft/Library/libft.h"
+# include "ft_Printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+
+#endif
